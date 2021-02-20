05d16f062573bb354754533f82abeddce228a1b1
MANDOM <yourmarshall@yandex.by>
2021-02-20 17:35:35 +0300

    Add js and api calls

--- SalesStatistics/SalesStatistics/Controllers/SalesController.cs
+++ SalesStatistics/SalesStatistics/Controllers/SalesController.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesStatistics.Models;
 
 namespace SalesStatistics.Controllers
 {
     public class SalesController : Controller
     {
+        private readonly SalesDbContext _db;
+
+        public SalesController(SalesDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Sales.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Sales.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Sales.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
         }
     }
 }
